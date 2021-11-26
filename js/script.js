c9abe251cfd0a7433c69364f0895a8fe21749b4c
Victor Maya <victor.maya42@gmail.com>
2021-11-26 18:32:09 -0300

    navegaçao pre e next finalizada

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-import Slide from './slide.js';
+import { Slide, SlideNav } from './slide.js';
 
-const slide = new Slide('.slide', '.wrapper');
+const slide = new SlideNav('.slide', '.wrapper');
 slide.init();
-slide.changeSlide(1);
+slide.addArrow('.prev', '.next');
